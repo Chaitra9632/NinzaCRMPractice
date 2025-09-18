898ba178b80323e1f6a5b5a53277ccb655b9f0c6
Chaitra9632 <chaitra.sreenivas88@gmail.com>
2025-09-18 13:48:58 +0530

    sample 5 committed

--- src/test/java/demo/Sample4.java
+++ src/test/java/demo/Sample4.java
@@ -1,11 +1,10 @@
 package demo;
 
-public class Sample3 {
-	
+public class Sample4 {
+
 	public static void main(String[] args) {
-		
-		System.out.println();
-		
+		// TODO Auto-generated method stub
+
 	}
 
 }
